7a33f65354f3c40aa158835df931d9dd4eeb8807
Marcos Carrasco <161668037+marcoscarrasc@users.noreply.github.com>
2025-03-11 09:20:08 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,12 @@ function App() {
       <h1 className="text-4xl text-center text-blue-500">
         Hello World
       </h1  >
+
+      <div className="flex justify-center mt-5">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+          Click Me
+        </button>
+      </div>
     </>
   )
 }
